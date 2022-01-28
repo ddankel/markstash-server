47df5a9d2fb51190a5ce0ee3bd564eea04ad3de3
Dave Dankel <ddankel@gmail.com>
2022-01-28 00:43:16 -0600

    attachCurrentUser middleware

---
+++ src/middlewares/pre_router/attach_current_user/index.test.js
@@ -0,0 +1,85 @@
+const attachCurrentUser = require("./index");
+const userFactory = require("../../../../tests/factories/user");
+const User = require("../../../models/user");
+
+// Setup for tests
+const auth0Id = "auth0|12345abcde";
+const payload = { sub: auth0Id };
+payload["http://org.auth0.markstash/username"] = "new name";
+
+beforeEach(() => {
+  mockRequest = {};
+  mockResponse = {};
+  mockNext = jest.fn();
+});
+
+describe("when req.auth.payload.sub is undefined", () => {
+  beforeEach(async () => {
+    mockRequest = { auth: { payload: {} } };
+    await attachCurrentUser(mockRequest, mockResponse, mockNext);
+  });
+
+  it("does not create a new user", async () => {
+    const subject = await (await User.query()).length;
+    expect(subject).toBe(0);
+  });
+
+  it("does not set req.current_user", () => {
+    expect(mockRequest.current_user).toBeUndefined();
+  });
+
+  it("calls next()", () => {
+    expect(mockNext).toBeCalledTimes(1);
+  });
+});
+
+describe("when the auth0Id does not match an existing user", () => {
+  beforeEach(async () => {
+    mockRequest = { auth: { payload } };
+    await attachCurrentUser(mockRequest, mockResponse, mockNext);
+  });
+
+  it("creates a new user", async () => {
+    const subject = await User.query().findOne({ auth0Id });
+    expect(subject).toMatchObject({
+      auth0Id,
+      username: "new name",
+    });
+  });
+
+  it("sets req.current_user", () => {
+    expect(mockRequest.current_user).toMatchObject({
+      auth0Id,
+      username: "new name",
+    });
+  });
+
+  it("calls next()", () => {
+    expect(mockNext).toBeCalledTimes(1);
+  });
+});
+
+describe("when the auth0Id matches an existing user", () => {
+  beforeEach(async () => {
+    mockRequest = { auth: { payload } };
+    user = await userFactory.create({ auth0Id });
+    await attachCurrentUser(mockRequest, mockResponse, mockNext);
+  });
+
+  it("updates the user's username", async () => {
+    const subject = await User.query().findById(user.id);
+    expect(subject.username).toBe("new name");
+  });
+
+  it("sets req.current_user", () => {
+    expect(mockRequest.current_user).toMatchObject({
+      id: user.id,
+      auth0Id: user.auth0Id,
+      username: "new name",
+    });
+  });
+
+  it("calls next()", () => {
+    expect(mockNext).toBeCalledTimes(1);
+  });
+});
